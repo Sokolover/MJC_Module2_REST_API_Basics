bced3016e53d6521363c3b63e6b7ab1ac23f6654
unknown <sa.sokoloff@mail.ru>
2020-10-27 22:47:22 +0300

    connection pool added

--- core/src/main/java/com/epam/esm/sokolov/config/SpringJdbcConfig.java
+++ core/src/main/java/com/epam/esm/sokolov/config/SpringJdbcConfig.java
@@ -1,9 +1,9 @@
 package com.epam.esm.sokolov.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -20,12 +20,15 @@ public class SpringJdbcConfig {
 
     @Profile("dev")
     @Bean
-    public DataSource mysqlDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    public BasicDataSource mysqlDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
         dataSource.setUrl(environment.getProperty("jdbc.url"));
         dataSource.setUsername(environment.getProperty("jdbc.user"));
         dataSource.setPassword(environment.getProperty("jdbc.password"));
+        dataSource.setMinIdle(Integer.parseInt(environment.getProperty("jdbc.minIdle")));
+        dataSource.setMaxIdle(Integer.parseInt(environment.getProperty("jdbc.maxIdle")));
+        dataSource.setMaxOpenPreparedStatements(Integer.parseInt(environment.getProperty("jdbc.maxOpenStatements")));
         return dataSource;
     }
 
